2b670db6810d43342734c1ec4c53758ab2a57945
VirtualGhost007 <kavitanipun@gmail.com>
2023-06-21 00:59:32 +0530

    calculator

---
+++ Calculator(P1)/Learning.py
@@ -0,0 +1,23 @@
+# Asking for input from user
+a=int(input('Enter first number: '))
+b=int(input('Enter second number: '))
+
+# Now will make a normal calculator
+
+# Answers of all functions present
+Ans1=a+b
+Ans2=a-b
+Ans3=a*b
+Ans4=a/b
+Ans5=a%b
+Ans6=a**b
+Ans7=a//b
+
+# Result output
+print('Add of',a,'and',b,'is',Ans1)
+print('Sub of',a,'and',b,'is',Ans2)
+print('Multiply of',a,'and',b,'is',Ans3)
+print('Divide of',a,'and',b,'is',Ans4)
+print('Modulus of',a,'and',b,'is',Ans5)
+print('Exponential of',a,'and',b,'is',Ans6)
+print('Floor divide of',a,'and',b,'is',Ans7)
